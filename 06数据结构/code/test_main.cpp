e8b5e3bb4122ddc090b494583893c94e920575e3
Oreoxp <992972774@qq.com>
2021-02-02 17:13:15 +0800

    add src&list

--- 06数据结构/code/test_main.cpp
+++ 06数据结构/code/test_main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include "XVector/XVector.cpp"
+#include "XList/XList.h"
 #include "XList/XList.cc"
 
 using namespace std;
@@ -8,7 +9,7 @@ using namespace std;
 int main(){
     cout  << "hello main ~~~~~!" << endl;
 
-    XVector<int> xv;
+    XList<int> xv;
 
     xv.printfAll();
 
