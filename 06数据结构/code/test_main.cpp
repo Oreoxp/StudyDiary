5828ab1be457f50ae3dd62c440d612e4ecb5ed30
Oreoxp <992972774@qq.com>
2021-03-17 18:34:42 +0800

    create bintree

--- 06数据结构/code/test_main.cpp
+++ 06数据结构/code/test_main.cpp
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <iostream>
-#include "XVector/XVector.cpp"
-#include "XList/XList.h"
-#include "XList/XList.cc"
+//#include "XVector/XVector.cpp"
+//#include "XList/XList.h"
+//#include "XList/XList.cc"
+#include "Xtree/XBinTree.h"
 
 using namespace std;
 
 int main(){
     cout  << "hello main ~~~~~!" << endl;
 
-    XList<int> xv;
+    /*XList<int> xv;
 
     xv.printfAll();
 
@@ -19,8 +20,43 @@ int main(){
         xv.push_back(i); 
     }
 
-    xv.printfAll();
+    xv.printfAll();*/
+        BinNode<string>* root = new BinNode<string>("A");
 
+        BinNode<string>* nodeB = new BinNode<string>("B");
+        BinNode<string>* nodeC = new BinNode<string>("C");
+        BinNode<string>* nodeD = new BinNode<string>("D");
+        BinNode<string>* nodeE = new BinNode<string>("E");
+        BinNode<string>* nodeF = new BinNode<string>("F");
+        root->left = nodeB;
+        root->right = nodeC;
+        nodeB->left = nodeD;
+        nodeB->right = nodeE;
+        nodeC->left = nodeF;
+        
+        cout  << "--------root--------" << root<< endl;
+        cout  << "--------nodeB--------" << nodeB<< endl;
+        cout  << "--------nodeC--------" << nodeC<< endl;
+        cout  << "--------nodeD--------" << nodeD<< endl;
+        cout  << "--------nodeE--------" << nodeE<< endl;
+        cout  << "--------nodeF--------" << nodeF<< endl;
+
+    cout  << "start oper ~~~~~!" << endl;
+
+        int height = hight(root);
+        std::cout  << "height: " << height << std::endl;
+
+        int size = msize(root);
+        std::cout  << "size: " << size << std::endl;
+
+        std::cout  << "--------preOrder--------" << std::endl;
+        preOrder(root);
+
+        std::cout  << "--------midOrder--------" << std::endl;
+        midOrder(root);
+
+        std::cout  << "--------postOrder-------" << std::endl;
+        postOrder(root);
     
     cout  << "bye main ~~~~~!" << endl;
     return 0;
