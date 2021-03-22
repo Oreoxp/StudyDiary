e5f63d3049d99d949b27dfe45616f1ec58275551
Oreoxp <992972774@qq.com>
2021-03-22 22:58:58 +0800

    create delete func

--- 06数据结构/code/XTree/XHeap.h
+++ 06数据结构/code/XTree/XHeap.h
@@ -35,12 +35,12 @@ MaxHeap Create(int maxsize) {
 //从新增的最后一个节点的父节点开始，用要插入的元素向上过滤上层节点
 bool insert(MaxHeap h, element x){
 
-    if(h->size >= h->capacity){
+    if(h->size == h->capacity){
         std::cout << "head is full" ;
         return false;
     }
 
-    int i = h->size ++;
+    int i = ++h->size ;
     for( ;h->data[i/2] < x; i/=2) {
         h->data[i] = h->data[i/2];
     }     
@@ -53,19 +53,35 @@ bool insert(MaxHeap h, element x){
 //从根节点开始，用最大堆中的最后一个元素向上过滤下层节点
 
 element deleteMax(MaxHeap h) {
-    int i = h->size ++;
+    int i = h->size;
     element preroot = h->data[1];
     h->data[1] = h->data[i];
     element root = h->data[1];
 
+    std::cout << endl;
+    std::cout << "deleteMax show: "<< endl;
     int j = 1;
-    for(; (h->data[j*2] > root || h->data[j*2+1] > root); j*=2) {
+    int size = --h->size;
+    int operbit = 1;
+    std::cout << "size: "<< size << endl;
+    for(; (j*2 < size || j*2+1 < size)&&(h->data[j*2] > root || h->data[j*2+1] > root);) {
         if(h->data[j*2] > root) {//左子节点
+            cout<<"left"<< j << ","
+            <<"h->data[j] = " << h->data[j] << ","
+            <<"h->data[j*2] = " << h->data[j*2]<<endl;
             h->data[j] = h->data[j*2];
+            operbit = j;
+            j*=2;
         }else if(h->data[j*2+1] > root){//右子节点
+            cout<<"right : j="<< j  << ","
+            <<"h->data[j] = " << h->data[j] << ","
+            <<"h->data[j*2+1] = " << h->data[j*2+1]<<endl;
             h->data[j] = h->data[j*2+1];
+            operbit = j;
+            j=j*2+1;
         }
     }     
+    cout<<"jump for operbit="<< operbit <<endl;
     h->data[j] = root;
 
     return preroot;
@@ -73,15 +89,15 @@ element deleteMax(MaxHeap h) {
 
 void showMaxHead(MaxHeap h) {
     
-    for(int i = 0; i < h->size; i++) {
+    /*for(int i = 0; i < h->size + 1; i++) {
         std::cout << h->data[i] << ",";
-    }
+    }*/
     std::cout << endl;
 
     std::cout << "Max head show: "<< endl;
-    for(int i = 1; i < h->size; i++) {
+    for(int i = 1; i < h->size+ 1; i++) {
         std::cout << h->data[i] << ",";
-        if ( i%2 == 0){
+        if ( i == 1 || i == 3 || i == 7 || i == 15){
             std::cout << endl;
         }
     }
