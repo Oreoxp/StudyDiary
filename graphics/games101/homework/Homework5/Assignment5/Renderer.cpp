f1934e7bf67620dbe67a9793c252d153a5159f28
Oreoxp <42205335+Oreoxp@users.noreply.github.com>
2023-03-03 18:32:38 +0800

    update renderer

--- graphics/games101/homework/Homework5/Assignment5/Renderer.cpp
+++ graphics/games101/homework/Homework5/Assignment5/Renderer.cpp
@@ -25,6 +25,18 @@ Vector3f reflect(const Vector3f &I, const Vector3f &N)
 // If the ray is outside, you need to make cosi positive cosi = -N.I
 //
 // If the ray is inside, you need to invert the refractive indices and negate the normal N
+// 
+// // 使用斯涅尔定律计算折射方向
+//
+// 我们需要小心处理两种可能的情况：
+//
+// - 当光线在物体内部时
+//
+// - 当光线在外面时。
+//
+// 如果射线在外面，则需要使 cosi 为正 cosi = -N.I
+//
+// 如果光线在内部，则需要反转折射率并取反法线 N
 // [/comment]
 Vector3f refract(const Vector3f &I, const Vector3f &N, const float &ior)
 {
@@ -45,6 +57,14 @@ Vector3f refract(const Vector3f &I, const Vector3f &N, const float &ior)
 // \param N is the normal at the intersection point
 //
 // \param ior is the material refractive index
+// 
+// // 计算菲涅尔方程
+//
+// \param I 是事件视图方向
+//
+// \param N 是交点处的法线
+//
+// \param ior 是材料的折射率
 // [/comment]
 float fresnel(const Vector3f &I, const Vector3f &N, const float &ior)
 {
@@ -79,6 +99,17 @@ float fresnel(const Vector3f &I, const Vector3f &N, const float &ior)
 // \param[out] uv stores the u and v barycentric coordinates of the intersected point
 // \param[out] *hitObject stores the pointer to the intersected object (used to retrieve material information, etc.)
 // \param isShadowRay is it a shadow ray. We can return from the function sooner as soon as we have found a hit.
+// 
+// // 如果光线与对象相交，则返回 true，否则返回 false。
+//
+// \param orig 是光线原点
+// \param dir 是光线方向
+// \param objects 是场景包含的对象列表
+// \param[out] tNear 包含到最近相交对象的距离。
+// \param[out] index 如果相交对象是网格，则存储相交三角形的索引。
+// \param[out] uv 存储相交点的 u 和 v 重心坐标
+// \param[out] *hitObject 存储指向相交对象的指针（用于检索材质信息等）
+// \param isShadowRay 是阴影射线。 一旦找到命中，我们就可以尽快从函数中返回。
 // [/comment]
 std::optional<hit_payload> trace(
         const Vector3f &orig, const Vector3f &dir,
@@ -120,6 +151,17 @@ std::optional<hit_payload> trace(
 //
 // If the surface is diffuse/glossy we use the Phong illumation model to compute the color
 // at the intersection point.
+// 
+// // Whitted 式光传输算法的实现 (E [S*] (D|G) L)
+//
+// 此函数是计算由位置和方向定义的光线交点处的颜色的函数。
+// 请注意，因此函数是递归的（它调用自身）。
+//
+// 如果相交物体的材质是反射或反射和折射，那么我们计算反射/折射方向，并通过递归调用
+// castRay() 函数将两条新光线投射到场景中。
+// 当表面透明时，我们使用菲涅尔方程的结果混合反射和折射颜色（它根据表面法线、入射观察方向和表面折射率计算反射和折射量）。
+//
+// 如果表面是漫反射/有光泽的，我们使用 Phong 照明模型来计算交点处的颜色。
 // [/comment]
 Vector3f castRay(
         const Vector3f &orig, const Vector3f &dir, const Scene& scene,
@@ -168,6 +210,8 @@ Vector3f castRay(
                 // [comment]
                 // We use the Phong illumation model int the default case. The phong model
                 // is composed of a diffuse and a specular reflection component.
+                // 
+                // 我们在默认情况下使用 Phong 光照模型。 phong 模型由漫反射和镜面反射组件组成。
                 // [/comment]
                 Vector3f lightAmt = 0, specularColor = 0;
                 Vector3f shadowPointOrig = (dotProduct(dir, N) < 0) ?
@@ -207,6 +251,9 @@ Vector3f castRay(
 // The main render function. This where we iterate over all pixels in the image, generate
 // primary rays and cast these rays into the scene. The content of the framebuffer is
 // saved to a file.
+// 
+// 主要的渲染函数。 在这里我们迭代图像中的所有像素，
+// 生成主光线并将这些光线投射到场景中。 帧缓冲区的内容被保存到一个文件中。
 // [/comment]
 void Renderer::Render(const Scene& scene)
 {
@@ -222,8 +269,21 @@ void Renderer::Render(const Scene& scene)
     for (int j = 0; j < scene.height; ++j) {
       for (int i = 0; i < scene.width; ++i) {
         // generate primary ray direction
+        float x, y;
+
+        //NDC 像素中心  ->  [0~1]
+        float centerX = (i + 0.5f) / scene.width;
+        float centerY = (j + 0.5f) / scene.height;
+        
+        //[0~1] -> [-1~1]
+        float pixelX = centerX * 2 - 1;
+        float pixelY = 1 - centerY * 2;
+
+        x = pixelX * scale * imageAspectRatio;
+        y = pixelY * scale;
         Vector3f dir =
             Vector3f(x, y, -1);  // Don't forget to normalize this direction!
+        dir = normalize(dir);
         framebuffer[m++] = castRay(eye_pos, dir, scene, 0);
       }
       UpdateProgress(j / (float)scene.height);
