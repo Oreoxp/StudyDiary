cf257f05fb03528071565007e82039fbc68a9ce8
Oreoxp <42205335+Oreoxp@users.noreply.github.com>
2023-03-08 20:23:35 +0800

    update qml

--- graphics/openglproject/QtQuickBasicGraphicsDrawing/fboinsgrenderer.cpp
+++ graphics/openglproject/QtQuickBasicGraphicsDrawing/fboinsgrenderer.cpp
@@ -1,43 +1,3 @@
-/****************************************************************************
-**
-** Copyright (C) 2015 The Qt Company Ltd.
-** Contact: http://www.qt.io/licensing/
-**
-** This file is part of the examples of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** You may use this file under the terms of the BSD license as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of The Qt Company Ltd nor the names of its
-**     contributors may be used to endorse or promote products derived
-**     from this software without specific prior written permission.
-**
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-
 #include "fboinsgrenderer.h"
 #include "logorenderer.h"
 
@@ -45,6 +5,11 @@
 
 #include <QtQuick/QQuickWindow>
 #include <qsgsimpletexturenode.h>
+
+#include <iostream>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 class LogoInFboRenderer : public QQuickFramebufferObject::Renderer
 {
@@ -90,44 +55,7 @@ GLFWRenderer::GLFWRenderer()
     : m_fbo(nullptr),
       m_vao(0),
       m_vbo(0), m_program(0) {
-    initializeOpenGLFunctions();  
-    // Make sure a valid OpenGL context is current
-    QOpenGLContext* ctx = QOpenGLContext::currentContext();
-    if (!ctx) {
-        qWarning() << "No current OpenGL context";
-        return;
-    }
-    if (!ctx->isValid()) {
-        qWarning() << "Invalid OpenGL context";
-        return;
-    }
-    // Create shader program
-    const char* vertexShaderSource =
-        "#version 330 core\n"
-        "layout(location = 0) in vec3 aPos;\n"
-        "void main()\n"
-        "{\n"
-        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-        "}\0";
-    const char* fragmentShaderSource =
-        "#version 330 core\n"
-        "out vec4 FragColor;\n"
-        "void main()\n"
-        "{\n"
-        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-        "}\n\0";
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
-    glCompileShader(vertexShader);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-    glCompileShader(fragmentShader);
-    m_program = glCreateProgram();
-    glAttachShader(m_program, vertexShader);
-    glAttachShader(m_program, fragmentShader);
-    glLinkProgram(m_program);
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    initializeOpenGLFunctions();
 }
 
 GLFWRenderer::~GLFWRenderer() {
@@ -166,24 +94,44 @@ void GLFWRenderer::render() {
         // Create shader program
         const char* vertexShaderSource =
             "#version 330 core\n"
-            "layout(location = 0) in vec3 aPos;\n"
+            "layout (location = 0) in vec3 aPos;\n"// 位置变量的属性位置值为 0
+            "layout (location = 1) in vec3 aColor;\n"// 颜色变量的属性位置值为 1
+            "layout(location = 2) in vec2 aTexCoord; \n"// 纹理变量的属性位置值为 1
+            "out vec4 vertexColor;\n"
+            "out vec4 ourPosition;\n"
+            "out vec2 TexCoord;\n" 
+            "uniform vec2 dev = vec2(0.5, 0);\n"
             "void main()\n"
             "{\n"
-            "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+            "   vertexColor = vec4(aColor, 1.0);\n"
+            "   gl_Position = vec4(aPos, 1.0);\n"
+            "   ourPosition = gl_Position;\n"
+            "   TexCoord = vec2(aTexCoord.x, aTexCoord.y); \n" 
             "}\0";
         const char* fragmentShaderSource =
             "#version 330 core\n"
             "out vec4 FragColor;\n"
+            "in vec4 vertexColor;\n"
+            "in vec4 ourPosition;\n"
+            "in vec2 TexCoord;\n"
             "void main()\n"
             "{\n"
-            "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+            "   FragColor = vertexColor;\n"
             "}\n\0";
+
         GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
         GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
         glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
         glCompileShader(vertexShader);
+
+        int success;
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
         glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
         glCompileShader(fragmentShader);
+
+        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
         m_program = glCreateProgram();
         glAttachShader(m_program, vertexShader);
         glAttachShader(m_program, fragmentShader);
@@ -192,9 +140,52 @@ void GLFWRenderer::render() {
         glDeleteShader(fragmentShader);
     }
 
+
+    if (!m_vao) {
+        // Create VAO and VBO
+        float vertices[] = {
+            //     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
+            0.5f,  0.5f,  0.0f,        1.0f, 0.0f, 0.0f,     1.0f, 1.0f,  // 右上
+            0.5f,  -0.5f, 0.0f,        0.0f, 1.0f, 0.0f,     1.0f, 0.0f,  // 右下
+           -0.5f, -0.5f, 0.0f,         0.0f, 0.0f, 1.0f,     0.0f, 0.0f,  // 左下
+           -0.5f, 0.5f,  0.0f,         1.0f, 1.0f, 0.0f,     0.0f, 1.0f   // 左上
+        };
+        unsigned int indices[] = {
+            0, 1, 3,  // first triangle
+            1, 2, 3   // second triangle
+        };
+        unsigned int  EBO;
+        glGenVertexArrays(1, &m_vao);
+        glGenBuffers(1, &m_vbo);
+        glGenBuffers(1, &EBO);
+
+
+        glBindVertexArray(m_vao);
+        glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
+                     GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+                     GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                              (void*)0);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                              (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                              (void*)(6 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+    }
+
+    
     // Render to FBO
     m_fbo->bind();
-
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
     glEnable(GL_MULTISAMPLE);
@@ -202,24 +193,9 @@ void GLFWRenderer::render() {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    if (!m_vao) {
-        // Create VAO and VBO
-        float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f,
-                            0.0f,  0.0f,  0.5f, 0.0f};
-        glGenVertexArrays(1, &m_vao);
-        glGenBuffers(1, &m_vbo);
-        glBindVertexArray(m_vao);
-        glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
-                     GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
-                              nullptr);
-        glEnableVertexAttribArray(0);
-    }
-    // Draw triangle
     glUseProgram(m_program);
     glBindVertexArray(m_vao);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     // Release FBO
     m_fbo->release();
