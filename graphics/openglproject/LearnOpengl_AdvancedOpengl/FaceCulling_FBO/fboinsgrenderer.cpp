91eb7a46c00b955e4000c9bd54f1196763a261cc
Oreoxp <42205335+Oreoxp@users.noreply.github.com>
2023-03-27 18:15:33 +0800

    设置纹理

--- graphics/openglproject/LearnOpengl_AdvancedOpengl/FaceCulling_FBO/fboinsgrenderer.cpp
+++ graphics/openglproject/LearnOpengl_AdvancedOpengl/FaceCulling_FBO/fboinsgrenderer.cpp
@@ -61,24 +61,12 @@ float vertices[] = {
     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
     -0.5f, 0.5f,  0.5f,  0.0f, 0.0f, -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f};
 
-GLuint initCubemapTexture() {
-  GLuint cubeMapTexture;
-  glGenTextures(1, &cubeMapTexture);
-  glBindTexture(GL_TEXTURE_CUBE_MAP, cubeMapTexture);
+float quadVertices[] = {  // vertex attributes for a quad that fills the entire
+                          // screen in Normalized Device Coordinates.
+    // positions   // texCoords
+    -1.0f, 1.0f, 0.0f, 1.0f, -1.0f, -1.0f, 0.0f, 0.0f, 1.0f, -1.0f, 1.0f, 0.0f,
 
-  for (unsigned int i = 0; i < 6; ++i) {
-    glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, 800, 800, 0,
-                 GL_RGB, GL_UNSIGNED_BYTE, nullptr);
-  }
-
-  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-
-  return cubeMapTexture;
-}
+    -1.0f, 1.0f, 0.0f, 1.0f, 1.0f,  -1.0f, 1.0f, 0.0f, 1.0f, 1.0f,  1.0f, 1.0f};
 
 GLFWRenderer::GLFWRenderer()
     : m_fbo(nullptr), m_vao(0), m_vbo(0), m_program(0) {
@@ -147,7 +135,7 @@ GLFWRenderer::GLFWRenderer()
   if (!m_vao) {
     glGenVertexArrays(1, &m_vao);
     glGenBuffers(1, &m_vbo);
-     
+
     glBindVertexArray(m_vao);
     glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
@@ -159,7 +147,52 @@ GLFWRenderer::GLFWRenderer()
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
                           (void*)(3 * sizeof(float)));
 
-    cubeMapTexture = initCubemapTexture();
+    glGenVertexArrays(1, &m_vao2);
+    glGenBuffers(1, &m_vbo2);
+
+    glBindVertexArray(m_vao2);
+    glBindBuffer(GL_ARRAY_BUFFER, m_vbo2);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices,
+                 GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float),
+                          (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float),
+                          (void*)(2 * sizeof(float)));
+
+
+
+
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    // create a color attachment texture
+    
+    glGenTextures(1, &textureColorbuffer);
+    glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 800, 800, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,
+                           textureColorbuffer, 0);
+    // create a renderbuffer object for depth and stencil attachment (we won't
+    // be sampling these)
+    unsigned int rbo;
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, 800,
+                          800);  // use a single renderbuffer object for
+                                 // both a depth AND stencil buffer.
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT,
+                              GL_RENDERBUFFER, rbo);  // now actually attach it
+    // now that we actually created the framebuffer and added all attachments we
+    // want to check if it is actually complete now
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+      std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!"
+                << std::endl;
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
   }
   timer.start();
 }
@@ -189,6 +222,7 @@ QOpenGLFramebufferObject* GLFWRenderer::createFramebufferObject(
 void GLFWRenderer::render() {
   // Render to first FBO
   {
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
     back_fbo->bind();
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
@@ -212,8 +246,6 @@ void GLFWRenderer::render() {
                        GL_FALSE, projection.data());
 
     glBindVertexArray(m_vao);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,
-                           cubeMapTexture, 0);
     QMatrix4x4 model;
     model.translate(QVector3D(0, 0, 0));
     glUniformMatrix4fv(glGetUniformLocation(m_program, "model"), 1, GL_FALSE,
@@ -221,23 +253,12 @@ void GLFWRenderer::render() {
 
     glDrawArrays(GL_TRIANGLES, 0, 36);
 
-
-    // 将 FBO 纹理转换为立方体纹理
-    glBindTexture(GL_TEXTURE_2D, cubeMapTexture);
-    for (unsigned int i = 0; i < 6; ++i) {
-      glCopyTexSubImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, 0, 0, 0, 0,
-                          800, 800);
-    }
-    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
-      qDebug() << "error";
-    }
     glBindVertexArray(0);
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
     glDisable(GL_DEPTH_TEST);
     back_fbo->release();
   }
 
-  m_fbo->bind();
   glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
@@ -246,31 +267,11 @@ void GLFWRenderer::render() {
   glDepthFunc(GL_LESS);
   glEnable(GL_MULTISAMPLE);
 
-  QMatrix4x4 view{};
-  QMatrix4x4 projection{};
-  QMatrix4x4 model;
-
-  view.lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
-  view.rotate(qRadiansToDegrees(20.0f), 1, 1, 0);
-  projection.perspective(qRadiansToDegrees(45.0f), (float)800 / (float)800,
-                         0.1f, 100.0f);
-  model.translate(QVector3D(0, 0, 0));
-
-  glBindVertexArray(m_vao);
-
   m_shader->bind();
-  m_shader->setUniformValue("view", view);
-  m_shader->setUniformValue("projection", projection);
-  m_shader->setUniformValue("model", model);
-  m_shader->setUniformValue("view_pos", view * QVector3D(0, 0, 0));
-  glActiveTexture(GL_TEXTURE0);
-  glBindTexture(GL_TEXTURE_CUBE_MAP, cubeMapTexture);
-  m_shader->setUniformValue("envMap", 0);
-  m_shader->setUniformValue("CameraPos", cameraPos);
-  m_shader->setUniformValue("LightPos", QVector3D(5.0f, 5.0f, 5.0f));
-  m_shader->setUniformValue("LightColor", QVector3D(1.0f, 1.0f, 1.0f));
-
-  glDrawArrays(GL_TRIANGLES, 0, 36);
+  glBindVertexArray(m_vao2);
+  glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+  m_shader->setUniformValue("screenTexture", 0);
+  glDrawArrays(GL_TRIANGLES, 0, 6);
   m_shader->release();
   /*
   // Render sphere with texture from first FBO
@@ -305,7 +306,7 @@ void GLFWRenderer::render() {
   // Release FBO
   m_fbo->release();
   */
-  glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
   glBindVertexArray(0);
   glDisable(GL_DEPTH_TEST);
   glDisable(GL_CULL_FACE);
--- graphics/openglproject/LearnOpengl_AdvancedOpengl/FaceCulling_FBO/sphere.vs
+++ graphics/openglproject/LearnOpengl_AdvancedOpengl/FaceCulling_FBO/sphere.vs
@@ -1,20 +1,11 @@
 #version 330 core
-layout (location = 0) in vec3 aPos;
+layout (location = 0) in vec2 aPos;
 layout (location = 1) in vec2 aTexCoords;
 
 out vec2 TexCoords;
 
-out vec3 WorldPos;
-out vec3 Normal;
-
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
-
 void main()
 {
-    gl_Position = projection * view * model * vec4(aPos, 1.0f);
-    WorldPos = vec3(model * vec4(aPos, 1.0));
-    //Normal = mat3(transpose(inverse(model))) * aNormal;
     TexCoords = aTexCoords;
-}
+    gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0); 
+}  
