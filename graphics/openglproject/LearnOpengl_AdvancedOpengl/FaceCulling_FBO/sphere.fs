4872e367247ac6083681c124f2085765e6efd329
Oreoxp <42205335+Oreoxp@users.noreply.github.com>
2023-03-29 11:57:19 +0800

    cube map  待完成

--- graphics/openglproject/LearnOpengl_AdvancedOpengl/FaceCulling_FBO/sphere.fs
+++ graphics/openglproject/LearnOpengl_AdvancedOpengl/FaceCulling_FBO/sphere.fs
@@ -5,10 +5,7 @@ in vec2 TexCoords;
 
 uniform sampler2D screenTexture;
 
-void main()
-{
+void main() {
     vec3 col = texture(screenTexture, TexCoords).rgb;
-    FragColor = texture(screenTexture, TexCoords);
-    float average = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
-    FragColor = vec4(average, average, average, 1.0);
+    FragColor = vec4(col, 1.0);
 } 
