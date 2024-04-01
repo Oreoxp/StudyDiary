f04cc58db0660e45f184254fd4f16c22a244d02d
Oreoxp <992972774@qq.com>
2024-04-01 12:12:50 +0800

    add  dui01

--- graphics/GUI/TEST/ConsoleApplication3/shader.fs
+++ graphics/GUI/TEST/ConsoleApplication3/shader.fs
@@ -2,10 +2,13 @@
 out vec4 FragColor;
 
 in vec3 ourColor;
+in vec2 TexCoord;
 
-uniform vec3 uColor;
+// texture samplers
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = vec4(uColor, 1.0f);
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.4);
 }
